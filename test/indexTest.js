3b875dc3996b9a128b982c26d2ba579fcec77433
cjbrock <corinna@flatironschool.com>
2018-11-15 12:45:52 -0500

    tests written

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,20 @@
-
-
 describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
+  describe("after index.js is processed", () => {
+    it("no longer has DOM node 'main#main'", () => {
+      expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+    });
 
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
+    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+      expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+    });
 
+    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+      expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+    });
 
-})
+    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+      expect(newHeader.innerText, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+    });
 
-
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
-  })
+  });
 })
