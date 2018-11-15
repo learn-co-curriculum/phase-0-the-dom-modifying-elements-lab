f5ff64f23361d2e8fb165e1115db4d50b6846654
cjbrock <corinna@flatironschool.com>
2018-11-15 12:48:34 -0500

    solution

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Flatiron School is the champion!";
+newHeader.className = "victory";
