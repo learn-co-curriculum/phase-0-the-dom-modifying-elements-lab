a337930756e1cd806422e4108e00c75bc5d79dfe
Kellye Greene <kellyecodes@gmail.com>
2019-04-15 17:14:55 -0400

    Updates test and solution for innerHTML

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerText = "Flatiron School is the champion!";
+newHeader.innerHTML = "Flatiron School is the champion!";
 newHeader.className = "victory";
