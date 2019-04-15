a661269b877ec32bd76d4dde5b70c6ba1beb1911
Kellye Greene <kellyecodes@gmail.com>
2019-04-15 17:16:02 -0400

    Updates test for innerHTML

--- test/indexTest.js
+++ test/indexTest.js
@@ -13,7 +13,7 @@ describe("index.html", () => {
     });
 
     it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
-      expect(newHeader.innerText, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
     });
 
   });
