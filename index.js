5474537f92fe73bc914ddc6865882ca5a347a32e
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-18 15:01:50 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+newHeader.innerHTML="<h1>YOUR-NAME is the champion</h2>";
+
