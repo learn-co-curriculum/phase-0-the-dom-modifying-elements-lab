2f1943f944f11e9d6745fe009b42d196b1ff9d31
Benjamin Alan Murphy-Kane <b3nmurphy924@gmail.com>
2022-03-23 14:00:30 -0700

    commit tree db2da6443d43a91ba4b1533001771657bce5b105

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ben is the champion!";
+newHeader.className = "victory";
