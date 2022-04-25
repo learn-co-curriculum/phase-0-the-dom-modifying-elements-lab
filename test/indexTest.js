f46868800cc2e493ed90ccce82557dcc9150f224
Gracie <m.graciemcguire@gmail.com>
2022-04-25 14:54:39 -0400

    clarifies test instructions

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,15 +4,21 @@ describe("index.html", () => {
       expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
-      expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+    it("has a 'newHeader' variable that points to an <h1> node, 'h1#victory'", () => {
+      expect(
+        newHeader.nodeName,
+        "Make sure you create an <h1> with id 'victory'"
+      ).eql("H1");
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
-      expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+    it("the 'newHeader' variable points to a node that has an id of 'victory'", () => {
+      expect(
+        newHeader.id,
+        "Make sure you create an <h1> with id 'victory'"
+      ).eql("victory");
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+    it("the 'newHeader' variable points to a node 'h1#victory', has \"YOUR-NAME is the champion\" inside", () => {
       expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
     });
 
