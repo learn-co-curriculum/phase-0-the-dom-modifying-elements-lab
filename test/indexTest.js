2484d6b618ee785fd7ba9b203324004de6f5441e
Ricardo Guerra <RikkuG491@gmail.com>
2022-08-10 14:39:18 -0400

    Updated wording and tests
    Updated the wording of the tests to make them more clear. Changed 'main' to 'main#main' in the first expect statement. Changed the last expect statement to require the newHeader.innerHTML to include the text "YOUR-NAME is the champion"

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,25 @@
 describe("index.html", () => {
   describe("after index.js is processed", () => {
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
     });
 
-    it("has a 'newHeader' variable that points to an <h1> node, 'h1#victory'", () => {
+    it("has a 'newHeader' variable that points to an <h1> node", () => {
       expect(
         newHeader.nodeName,
-        "Make sure you create an <h1> with id 'victory'"
+        "Make sure you create an <h1> and assign it to the 'newHeader' variable"
       ).eql("H1");
     });
 
-    it("the 'newHeader' variable points to a node that has an id of 'victory'", () => {
+    it("the 'newHeader' variable that points to the <h1> node has an id of 'victory'", () => {
       expect(
         newHeader.id,
-        "Make sure you create an <h1> with id 'victory'"
+        "Make sure your <h1> sets its id to 'victory'"
       ).eql("victory");
     });
 
-    it("the 'newHeader' variable points to a node 'h1#victory', has \"YOUR-NAME is the champion\" inside", () => {
-      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+    it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" inside it", () => {
+      expect(newHeader.innerHTML, "Make sure your <h1> with id 'victory' has the text \"YOUR-NAME is the champion\" inside it").to.include("YOUR-NAME is the champion");
     });
 
   });
