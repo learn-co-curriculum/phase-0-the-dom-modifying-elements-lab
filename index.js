71545578c42d039c547945ffca4660487c0147b3
Cesar Hernandez <chema27281@gmail.com>
2022-09-21 14:52:35 -0400

    commit tree d983c523bc0156496d3097d802b5a762c3a33cc1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id='victory';
+  const h1 = document.getElementById('victory');
+ newHeader.textContent ="Manny is the champion";
+document.querySelector('body').append(newHeader)
+
+
+
+
+
+
+
