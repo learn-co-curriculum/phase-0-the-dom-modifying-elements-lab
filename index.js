fcc198fd2e4960fc4683c786e11c37805fa97b95
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-01-11 15:57:27 -0700

    labs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'we is the champion';
+document.body.append(newHeader);
