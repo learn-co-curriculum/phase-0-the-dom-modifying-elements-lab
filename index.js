cb94eea7bb2bf57e8f2d2ac981ff2328422d1b90
Kyle Schneider <kyle.v.schneider.251@gmail.com>
2023-01-11 16:16:25 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//1
+const main = document.getElementById('main');
+main.remove();
+
+//2
+const newHeader = document.createElement('h1');
+
+//3
+newHeader.id = 'victory'
+
+//4
+newHeader.textContent = 'Our Name is the champion';
+
