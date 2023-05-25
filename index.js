c4218f4a235d8894d03b2672332a6654ac32d2de
Shanley Caswell <shanleycaswell@gmail.com>
2023-05-25 10:35:30 -0400

    dom modifying new push

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector('#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id' , 'victory')
+newHeader.innerHTML = ("our team is the champion!")
