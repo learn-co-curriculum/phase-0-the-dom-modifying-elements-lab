781ec5e6edd7ae503eee67f989f86c263df9082c
Sean <ohsean812@gmail.com>
2023-06-19 00:55:56 -0400

    commit tree 2d9f1e1c880cef9146a67eed8a8851476e5f3022

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here!
-const eraser = document.querySelector('main');
-eraser.remove();
+const main = document.getElementById("main")
+main.remove();
 
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
 
-document.body.append(newHeader.textContent = "Sean is the champion");
+newHeader.textContent = "Sean is the champion"
