fab739782d0cdefd8298f777b065606ce4c18c60
Sean <ohsean812@gmail.com>
2023-06-18 23:36:26 -0400

    commit tree 709fcd0987381f6e452ccf86eed6e78c2c6e6cf1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const eraser = document.querySelector('main');
+eraser.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+document.body.append(newHeader.textContent = "Sean is the champion");
